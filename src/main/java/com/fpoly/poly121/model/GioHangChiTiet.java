08a794544c19d4a3e28969016da08661fd4eb52f
Shiurd <hackersky09@outlook.com.vn>
2023-10-19 20:59:48 +0700

    Commit CRUD Model

---
+++ src/main/java/com/fpoly/poly121/model/GioHangChiTiet.java
@@ -0,0 +1,31 @@
+package com.fpoly.poly121.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "gio_hang_chi_tiet")
+public class GioHangChiTiet {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_gio_hang")
+    private GioHang idGioHang;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_san_pham_chi_tiet")
+    private SanPhamChiTiet idSanPhamChiTiet;
+
+    @Column(name = "so_luong")
+    private Integer soLuong;
+
+}
