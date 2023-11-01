30f6f5a7319686e8c50d99795fe89c42e09bc33a
nguyeenxtungf03 <nguyeenx.tungf03@gmail.com>
2023-11-01 21:07:01 +0700

    fix

---
+++ src/main/java/com/fpoly/poly121/service/ThuongHieuService.java
@@ -0,0 +1,22 @@
+package com.fpoly.poly121.service;
+
+import com.fpoly.poly121.model.ThuongHieu;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface ThuongHieuService {
+
+    Page<ThuongHieu> phanTrang(Integer pageNo, Integer i);
+
+    void add(ThuongHieu thuongHieu);
+
+        List<ThuongHieu> timKiem(String ten);
+    Page<ThuongHieu> phanTrangTimKiem(Integer pageNo, Integer size,String ten);
+
+    ThuongHieu getById(Long id);
+
+    void update(ThuongHieu thuongHieu);
+
+    void delete(Long id);
+}
